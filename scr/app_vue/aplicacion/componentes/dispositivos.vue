f48b43629f971e2f46eaea0a1bae1efdaf60eb97
jorge <jorge.meza.rdz@gmail.com>
2019-11-27 11:14:23 -0600

    config dps

--- scr/app_vue/aplicacion/componentes/dispositivos.vue
+++ scr/app_vue/aplicacion/componentes/dispositivos.vue
@@ -6,8 +6,8 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">ID</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">agua001-jamrelectronics</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">agua001-jamrelectronics</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">activo</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-microchip fa-2x text-gray-300"></i>
