8fce0a530cc2ede055888153a4b478abdbfa909f
jorge <jorge.meza.rdz@gmail.com>
2019-11-27 12:06:38 -0600

    config dps

--- scr/app_vue/aplicacion/componentes/dispositivos.vue
+++ scr/app_vue/aplicacion/componentes/dispositivos.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
   
-     <div v-for="item in dispositivos" :key="item" >
-       <div class="col-xl-3 col-md-6 mb-4">
+     
+       <div v-for="item in dispositivos" :key="item" class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -17,8 +17,8 @@
                   </div>
                 </div>
               </div>
-            </div>
-     </div>
+        </div>
+     
 
   </div>
 </template>
