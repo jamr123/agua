ac20a43e8777b8f563bd21f8091329ba68da3489
jorge <jorge.meza.rdz@gmail.com>
2019-11-27 12:09:00 -0600

    config dps

--- scr/app_vue/aplicacion/componentes/dispositivos.vue
+++ scr/app_vue/aplicacion/componentes/dispositivos.vue
@@ -2,7 +2,7 @@
   <div>
   
      
-       <div v-for="item in dispositivos" :key="item" class="col-xl-3 col-md-6 mb-4">
+       <div v-for="item in dpsall" :key="item" class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -37,7 +37,9 @@ export default {
     };
   },
   computed: {
-
+      dpsall:function(){
+        return this.dispositivos;
+      }
   },
   methods: {
  
