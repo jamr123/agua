98296b67f3f6afcd22fc785b16461e66a4e9a087
jorge <jorge.meza.rdz@gmail.com>
2019-11-27 12:55:46 -0600

    config dps

--- scr/app_vue/aplicacion/componentes/dispositivos.vue
+++ scr/app_vue/aplicacion/componentes/dispositivos.vue
@@ -8,7 +8,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-primary mb-1">Tipo: {{item.tipo}} </div>
-                      <div class="text-xs font-weight-bold text-primary mb-1">  Id: {{item.tipo}} </div>
+                      <div class="text-xs font-weight-bold text-primary mb-1">  Id: {{item.id}} </div>
                       
                       <div class="h5 mb-0 font-weight-bold text-gray-800">{{item.act}}</div>
                     </div>
