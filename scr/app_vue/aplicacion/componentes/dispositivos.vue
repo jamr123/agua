6850d499dc818fb7ab93e0f8cd8bbbc9c63973f7
jorge <jorge.meza.rdz@gmail.com>
2019-11-27 12:04:10 -0600

    config dps

--- scr/app_vue/aplicacion/componentes/dispositivos.vue
+++ scr/app_vue/aplicacion/componentes/dispositivos.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
   
-     <div class="col-xl-3 col-md-6 mb-4">
+     <div v-for="item in dispositivos" :key="item" >
+       <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary mb-1">agua001-jamrelectronics</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">Activo</div>
+                      <div class="text-xs font-weight-bold text-primary mb-1">item.tipo</div>
+                      <div class="text-xs font-weight-bold text-primary mb-1">item.id</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">item.act</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-microchip fa-2x text-gray-300"></i>
@@ -16,6 +18,7 @@
                 </div>
               </div>
             </div>
+     </div>
 
   </div>
 </template>
@@ -29,7 +32,7 @@ export default {
   name: "Dispositivos",
   data() {
     return {
-
+       dispositivos:[]
 
     };
   },
@@ -43,9 +46,10 @@ export default {
   mounted: function() {
    cliente.getAllDps({ token: localStorage.getItem("session"), data: "admin" })
    .then(res=>{
-     console.log(res);
+     console.log(res.data);
+     this.dispositivos=res.data;
 
-   })
+   });
    
   },
   created: function() {
